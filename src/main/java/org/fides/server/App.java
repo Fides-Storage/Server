a40e7529e352b774c719de052b251230cac1eeed
Niels ten Hove <ntenhove@hotmail.com>
2014-11-17 13:50:40 +0100

    Implementation of the serversocket and checkstyle updates

--- src/main/java/org/fides/server/App.java
+++ src/main/java/org/fides/server/App.java
@@ -1,13 +1,50 @@
 package org.fides.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
- * Hello world!
- *
+ * 
+ * @author Niels and Jesse
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	private static final int PORT = 4444;
+
+	/**
+	 * Main class of the Fides Server
+	 * 
+	 * @param args
+	 *            unused
+	 */
+	public static void main(String[] args) {
+		ServerSocket listener = null;
+		try {
+			listener = new ServerSocket(PORT);
+			Socket server;
+
+			while (true) {
+
+				server = listener.accept();
+				ClientConnection client = new ClientConnection(server);
+				Thread t = new Thread(client);
+				t.start();
+			}
+		}
+		catch (IOException e) {
+			System.out.println("IOException on socket listen: " + e.getMessage());
+
+		}
+		finally {
+
+			try {
+				listener.close();
+			}
+			catch (IOException e) {
+				System.out.println("IOException on socket listen: " + e.getMessage());
+
+			}
+		}
+	}
 }
--- src/main/java/org/fides/server/ClientConnection.java
+++ src/main/java/org/fides/server/ClientConnection.java
@@ -1,9 +1,60 @@
 package org.fides.server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 import org.fides.server.files.UserFile;
 
-public class ClientConnection {
+/**
+ * 
+ * @author Niels and Jesse
+ * 
+ *         Runnable to create a thread for the handling of a client
+ */
+public class ClientConnection implements Runnable {
 
+	// TODO
 	private UserFile userFile;
 
+	private Socket server;
+
+	/**
+	 * Constructor for client connection
+	 * 
+	 * @param server
+	 *            socket for the connection with the client
+	 */
+	public ClientConnection(Socket server) {
+		this.server = server;
+	}
+
+	/**
+	 * 
+	 */
+	public void run() {
+
+		try {
+			// Get input from the client
+
+			System.out.println("Waiting for client on port " +
+				server.getLocalPort() + "...");
+			System.out.println("Just connected to "
+				+ server.getRemoteSocketAddress());
+			DataInputStream in =
+				new DataInputStream(server.getInputStream());
+			System.out.println(in.readUTF());
+			DataOutputStream out =
+				new DataOutputStream(server.getOutputStream());
+			out.writeUTF("Thank you for connecting to "
+				+ server.getLocalSocketAddress() + "\nGoodbye!");
+
+			server.close();
+		}
+		catch (IOException ioe) {
+			System.out.println("IOException on socket listen: " + ioe);
+			ioe.printStackTrace();
+		}
+	}
 }
