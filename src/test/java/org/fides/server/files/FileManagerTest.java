039d3993690f51fce2b2577dcee9a877716709f5
Thijs ter Haar <thijsterhaar@home.nl>
2014-11-19 21:15:05 +0100

    FS23: Added PowerMockito and also mocked the PropertiesManager in the FileManagerTest, forcing the FileManager tests to use a test data directory.

--- src/test/java/org/fides/server/files/FileManagerTest.java
+++ src/test/java/org/fides/server/files/FileManagerTest.java
@@ -12,12 +12,20 @@ import java.nio.file.Paths;
 import org.apache.commons.io.FileUtils;
 import org.fides.server.tools.PropertiesManager;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
  * The JUnit Test Case for the FileManager
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(PropertiesManager.class)
 public class FileManagerTest {
 
 	private static final byte[] MESSAGE = ("DEFAULT MESSAGE: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore "
@@ -31,7 +39,9 @@ public class FileManagerTest {
 
 	private static final String DEFAULTREMOVEFILELOCATION = "defaultRemoveFile.txt";
 	
-	private static final File DATADIR = new File(PropertiesManager.getInstance().getDataDir());
+	private static File testDataDir;
+	
+	private static PropertiesManager mockedPropertiesManager = Mockito.mock(PropertiesManager.class);
 	
 	/**
 	 * Sets up the test class by adding a the necessary temporary files to the test folder.
@@ -39,15 +49,22 @@ public class FileManagerTest {
 	@BeforeClass
 	public static void setUp() {
 		try {
-			File emptyFile = new File(DATADIR, DEFAULTEMPTYFILELOCATION);
+			testDataDir = new File(PropertiesManager.getInstance().getDataDir(), "Test");
+			if (!testDataDir.exists()) {
+				testDataDir.mkdirs();
+			}
+			// This causes the mocked PropertiesManager to always return the test Data directory:
+			Mockito.when(mockedPropertiesManager.getDataDir()).thenReturn(testDataDir.getAbsolutePath());
+			
+			File emptyFile = new File(testDataDir, DEFAULTEMPTYFILELOCATION);
 			emptyFile.createNewFile();
 
-			File defaultFile = new File(DATADIR, DEFAULTFILELOCATION);
+			File defaultFile = new File(testDataDir, DEFAULTFILELOCATION);
 			FileOutputStream outputStream = new FileOutputStream(defaultFile);
 			outputStream.write(MESSAGE);
 			outputStream.close();
 
-			File removeFile = new File(DATADIR, DEFAULTREMOVEFILELOCATION);
+			File removeFile = new File(testDataDir, DEFAULTREMOVEFILELOCATION);
 			outputStream = new FileOutputStream(removeFile);
 			outputStream.write(MESSAGE);
 			outputStream.close();
@@ -55,7 +72,17 @@ public class FileManagerTest {
 			fail("Unexpected error in setUp: " + e.getMessage());
 		}
 	}
-
+	
+	/**
+	 * Mocks the PropertiesManager to always return a mocked version of the PropertiesManager.
+	 * This will cause the FileManager to use a testfolder instead of the main folder.
+	 */
+	@Before
+	public void setUpMock() {
+		PowerMockito.mockStatic(PropertiesManager.class);
+		Mockito.when(PropertiesManager.getInstance()).thenReturn(mockedPropertiesManager);
+	}
+	
 	/**
 	 * Tests if the manager correctly creates a new file.
 	 */
@@ -65,7 +92,7 @@ public class FileManagerTest {
 			String fileName = FileManager.createFile();
 
 			// Check if the file was created
-			assertTrue(Files.exists(Paths.get(DATADIR.getCanonicalPath(), fileName)));
+			assertTrue(Files.exists(Paths.get(testDataDir.getCanonicalPath(), fileName)));
 		} catch (Exception e) {
 			fail("An unexpected exception has occured: " + e.getMessage());
 		}
@@ -79,7 +106,7 @@ public class FileManagerTest {
 		try {
 			ByteArrayInputStream inputStream = new ByteArrayInputStream(MESSAGE);
 			assertTrue(FileManager.updateFile(inputStream, DEFAULTEMPTYFILELOCATION));
-			assertArrayEquals(MESSAGE, FileUtils.readFileToByteArray(new File(DATADIR, DEFAULTEMPTYFILELOCATION)));
+			assertArrayEquals(MESSAGE, FileUtils.readFileToByteArray(new File(testDataDir, DEFAULTEMPTYFILELOCATION)));
 		} catch (Exception e) {
 			fail("An unexpected exception has occured: " + e.getMessage());
 		}
@@ -94,7 +121,7 @@ public class FileManagerTest {
 			byte[] newMessage = "This file has been updated correctly.".getBytes();
 			ByteArrayInputStream inputStream = new ByteArrayInputStream(newMessage);
 			assertTrue(FileManager.updateFile(inputStream, DEFAULTFILELOCATION));
-			assertArrayEquals(newMessage, FileUtils.readFileToByteArray(new File(DATADIR, DEFAULTFILELOCATION)));
+			assertArrayEquals(newMessage, FileUtils.readFileToByteArray(new File(testDataDir, DEFAULTFILELOCATION)));
 		} catch (Exception e) {
 			fail("An unexpected exception has occured: " + e.getMessage());
 		}
@@ -115,9 +142,9 @@ public class FileManagerTest {
 	@Test
 	public void testRemoveFile() {
 		try {
-			assertTrue(Files.exists(Paths.get(DATADIR.getCanonicalPath(), DEFAULTREMOVEFILELOCATION)));
+			assertTrue(Files.exists(Paths.get(testDataDir.getCanonicalPath(), DEFAULTREMOVEFILELOCATION)));
 			assertTrue(FileManager.removeFile(DEFAULTREMOVEFILELOCATION));
-			assertFalse(Files.exists(Paths.get(DATADIR.getCanonicalPath(), DEFAULTREMOVEFILELOCATION)));
+			assertFalse(Files.exists(Paths.get(testDataDir.getCanonicalPath(), DEFAULTREMOVEFILELOCATION)));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -137,7 +164,7 @@ public class FileManagerTest {
 	@AfterClass
 	public static void tearDown() {
 		try {
-			FileUtils.cleanDirectory(DATADIR);
+			FileUtils.deleteDirectory(testDataDir);
 		} catch (Exception e) {
 			fail("Unexpected error in tearDown: " + e.getMessage());
 		}
