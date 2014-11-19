e921cc02122a49c6dfdcfbc519d27a1a5ecc9016
Niels ten Hove <ntenhove@hotmail.com>
2014-11-19 09:43:15 +0100

    config file added, deleted user test class

--- src/main/java/org/fides/server/App.java
+++ src/main/java/org/fides/server/App.java
@@ -1,6 +1,10 @@
 package org.fides.server;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * 
@@ -8,7 +12,7 @@ import java.io.IOException;
  * 
  */
 public class App {
-	private static final int PORT = 4444;
+	private static int port;
 
 	/**
 	 * Main class of the Fides Server
@@ -17,9 +21,26 @@ public class App {
 	 *            unused
 	 */
 	public static void main(String[] args) {
+		Properties properties = new Properties();
+		String propertiesFile = "config.properties";
+		try {
+			InputStream in = new FileInputStream(propertiesFile);
+			properties.load(in);
+			port = Integer.parseInt(properties.getProperty("port"));
+		}
+		catch (FileNotFoundException e) {
+			System.exit(1);
+		}
+		catch (IOException e) {
+			System.exit(1);
+		}
+		finally {
+
+		}
+
 		Server server;
 		try {
-			server = new Server(PORT);
+			server = new Server(port);
 			Thread serverThread = new Thread(server);
 			serverThread.start();
 		}
--- src/main/java/org/fides/server/User.java
+++
@@ -1,9 +0,0 @@
-package org.fides.server;
-
-public class User {
-
-	public String username;
-
-	public String password;
-
-}
