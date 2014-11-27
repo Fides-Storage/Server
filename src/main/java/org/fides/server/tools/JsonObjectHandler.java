0a4bd270b7fd8e42772ddd3d9924c90b371d8c69
Jesse Kerkhoven <jesse.kerkhoven@gmail.com>
2014-11-27 14:05:10 +0100

    Added javadoc to jsonobjecthandler

--- src/main/java/org/fides/server/tools/JsonObjectHandler.java
+++ src/main/java/org/fides/server/tools/JsonObjectHandler.java
@@ -2,12 +2,27 @@ package org.fides.server.tools;
 
 import com.google.gson.JsonObject;
 
-//TODO Add Javadoc to this class
+/**
+ * Helper for jsonobject
+ * 
+ * @author niels
+ * @author jesse
+ *
+ */
 public final class JsonObjectHandler {
 
 	private JsonObjectHandler() {
 	}
 
+	/**
+	 * Get property from jsonobject
+	 * 
+	 * @param jobj
+	 *            jsonobject where to property is taken from
+	 * @param property
+	 *            name to get property
+	 * @return the value of the given property
+	 */
 	public static String getProperty(JsonObject jobj, String property) {
 
 		if (jobj.has(property)) {
