e9cc95bc63d63ebc204cc864868bb4d5d7981c52
Thijs ter Haar <thijsterhaar@home.nl>
2014-12-16 09:54:49 +0100

    FS-28: Added some documentation and removed some temporary logging.

--- src/main/java/org/fides/server/tools/UserLocker.java
+++ src/main/java/org/fides/server/tools/UserLocker.java
@@ -4,7 +4,10 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
 
-public class UserLocker {
+/**
+ * A static tool that can be used to lock a user on the server. TODO: Test
+ */
+public final class UserLocker {
 
 	private static final FilenameFilter LOCKFILTER = new FilenameFilter() {
 		@Override
@@ -13,24 +16,39 @@ public class UserLocker {
 		}
 	};
 
+	private UserLocker() {
+	}
+
+	/**
+	 * Tries to lock the user with the given username
+	 * 
+	 * @param username
+	 *            The user that tries to be locked
+	 * @return Whether the lock was possible and successful
+	 */
 	public static boolean lock(String username) {
 		try {
 			File lockFile = new File(PropertiesManager.getInstance().getUserDir(), username + ".lock");
-			boolean locked = lockFile.createNewFile();
-			if (locked) {
-				System.out.println("=== USERFILE LOCKED ===");
-			}
-			return locked;
+			return lockFile.createNewFile();
 		} catch (IOException e) {
 			return false;
 		}
 	}
 
+	/**
+	 * Removes the lock of the user with the given username
+	 * 
+	 * @param username
+	 *            The user that can be unlocked
+	 */
 	public static void unlock(String username) {
 		File lockFile = new File(PropertiesManager.getInstance().getUserDir(), username + ".lock");
-		System.out.println("=== USERFILE UNLOCKED " + lockFile.delete() + " ===");
+		lockFile.delete();
 	}
 
+	/**
+	 * Clears all the available locks on the server. Should only be called on server startup.
+	 */
 	public static void clearAllLocks() {
 		File directory = new File(PropertiesManager.getInstance().getUserDir());
 		File[] lockFiles = directory.listFiles(LOCKFILTER);
