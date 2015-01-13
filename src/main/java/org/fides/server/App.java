e08bc058142bb2f3fe31ba0f42ae0e63b9156d67
Tom Kostense <tom.kostense@gmail.com>
2015-01-13 11:03:21 +0100

    ASCII logo on start

--- src/main/java/org/fides/server/App.java
+++ src/main/java/org/fides/server/App.java
@@ -15,7 +15,7 @@ public class App {
 	/**
 	 * Log for this class
 	 */
-	private static Logger log = LogManager.getLogger(App.class);
+	private static final Logger log = LogManager.getLogger(App.class);
 
 	/**
 	 * Main class of the Fides Server
@@ -25,6 +25,7 @@ public class App {
 	 */
 	public static void main(String[] args) {
 		Server server;
+		drawLogo();
 		log.trace("Starting server");
 		try {
 			server = new Server();
@@ -39,5 +40,26 @@ public class App {
 		long timeToClean = TimeUnit.DAYS.toMillis(1);
 		timer.scheduleAtFixedRate(new CleanerTask(), 0, timeToClean);
 
+	}
+
+	private static void drawLogo() {
+		log.info("\n"
+			+ "                                          dddddddd                                     \n"
+			+ "FFFFFFFFFFFFFFFFFFFFFF  iiii              d::::::d                                     \n"
+			+ "F::::::::::::::::::::F i::::i             d::::::d                                     \n"
+			+ "F::::::::::::::::::::F  iiii              d::::::d                                     \n"
+			+ "FF::::::FFFFFFFFF::::F                    d:::::d                                      \n"
+			+ "  F:::::F       FFFFFFiiiiiii     ddddddddd:::::d     eeeeeeeeeeee        ssssssssss   \n"
+			+ "  F:::::F             i:::::i   dd::::::::::::::d   ee::::::::::::ee    ss::::::::::s  \n"
+			+ "  F::::::FFFFFFFFFF    i::::i  d::::::::::::::::d  e::::::eeeee:::::eess:::::::::::::s \n"
+			+ "  F:::::::::::::::F    i::::i d:::::::ddddd:::::d e::::::e     e:::::es::::::ssss:::::s\n"
+			+ "  F:::::::::::::::F    i::::i d::::::d    d:::::d e:::::::eeeee::::::e s:::::s  ssssss \n"
+			+ "  F::::::FFFFFFFFFF    i::::i d:::::d     d:::::d e:::::::::::::::::e    s::::::s      \n"
+			+ "  F:::::F              i::::i d:::::d     d:::::d e::::::eeeeeeeeeee        s::::::s   \n"
+			+ "  F:::::F              i::::i d:::::d     d:::::d e:::::::e           ssssss   s:::::s \n"
+			+ "FF:::::::FF           i::::::id::::::ddddd::::::dde::::::::e          s:::::ssss::::::s\n"
+			+ "F::::::::FF           i::::::i d:::::::::::::::::d e::::::::eeeeeeee  s::::::::::::::s \n"
+			+ "F::::::::FF           i::::::i  d:::::::::ddd::::d  ee:::::::::::::e   s:::::::::::ss  \n"
+			+ "FFFFFFFFFFF           iiiiiiii   ddddddddd   ddddd    eeeeeeeeeeeeee    sssssssssss    \n");
 	}
 }
